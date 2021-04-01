0a7f14c9e5cf1d381adba643841c31b5f383c485
Debojyotibabai <debojyotighosh060@gmail.com>
2021-04-01 22:47:34 +0530

    cart currency change

--- src/Component/CartCard.js
+++ src/Component/CartCard.js
@@ -9,7 +9,7 @@ const CartCard = (props) => {
       <img src={props.img} alt="" />
       <div>
         <h1>{props.name}</h1>
-        <h2>{props.price}</h2>
+        <h2>₹ {Math.round(Number(props.price) * 75)}</h2>
         <button onClick={props.deleteCartCard}>REMOVE</button>
       </div>
     </div>
